d4c653034283ae1a288c394e45c4fa662f786dbc
brunty <matt@mfyu.co.uk>
2016-04-30 13:35:28 +0100

    Adds in app_test front controller

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProductController extends Controller
 {
@@ -19,6 +20,10 @@ class ProductController extends Controller
     public function viewProductAction($id)
     {
         $product = $this->get('doctrine.orm.default_entity_manager')->getRepository(Product::class)->find($id);
+
+        if ( ! $product) {
+            throw new NotFoundHttpException('Could not find the product');
+        }
 
         return $this->render(
             'Product/view.html.twig',
